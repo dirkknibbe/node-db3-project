7724bb1ecdf29cd26567798fe4a54bbb674f6b9d
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-03-23 21:47:53 -0600

    ok finished one query

--- queries.sql
+++ queries.sql
@@ -2,6 +2,9 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+select productName, categoryName from product
+join category on product.categoryid = category.id
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
